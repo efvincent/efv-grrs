a7020da0b1873d6837b6e6cbef1ec8346ec3c4f6
Eric Vincent <eric@efvincent.com>
2019-09-02 03:30:14 -0400

    test failing. App works

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,49 @@
 use structopt::StructOpt;
 use std::fs::File;
 use std::io::{BufRead,BufReader};
+use failure::ResultExt;
+use exitfailure::ExitFailure;
+use std::iter::Iterator;
+use log::{info};
 
-#[derive(StructOpt)]
+#[derive(StructOpt, Debug)]
 struct Cli {
     pattern: String,
     #[structopt(parse(from_str))]
     path: std::path::PathBuf
 }
 
-fn main() {
+fn find_matches(lines: impl IntoIterator<Item=String> , pattern: &str, mut writer: impl std::io::Write) -> ()
+{
+    for line in lines.into_iter() {
+        if line.contains(pattern) {
+            writeln!(writer, "{}", line);
+        }
+    }
+}
+
+#[test]
+fn can_find_match() {
+    let mut result = Vec::new();
+    find_matches("abc\ndef\nghi\njkl".lines(), "ef", &mut result);
+    assert_eq!(result, b"def");
+}
+
+fn main() -> Result<(), ExitFailure> {
+    env_logger::init();
+    info!("Starting up!");
+
     let args = Cli::from_args();
-    let f = File::open(&args.path).expect("Could not find file");
+    info!("args: {:?}", args);
+
+    // Note: `with_context` is a trait (extension) that turns an Err() result into
+    // an error type with a human readable context. The ? operator matches on error
+    // and causes main() to return the Result<> type we see in the decl. the main
+    // program must therefore return Ok(()). Note the inner type () is like unit, and
+    // there is no semicolon on the returned expression line in the function. Could also
+    // have used the keyword return Ok(()).
+    let f = File::open(&args.path)
+        .with_context(|_| format!("could not read file {:?}", &args.path))?;
     let reader = BufReader::new(f);
     /*
     Notes:
@@ -22,9 +54,8 @@ fn main() {
      * `unwrap_or_default()` returns the value associated with an Ok(T) result, or the
     empty string. This effectively sinks errors.
      */
-    for line in reader.lines().map(|l| l.unwrap_or_default()) {
-        if line.contains(&args.pattern) {
-            println!("{}", line);
-        }
-    }
+
+    find_matches(reader.lines().map(|l| l.unwrap_or_default()), &args.pattern, std::io::stdout());
+
+    Ok(())
 }
